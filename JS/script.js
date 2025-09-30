0ebc519340ce27c9b11c8375397ff3975adbee42
AngelinaBecker1 <angelina.becker@escola.pr.gov.br>
2025-09-30 12:01:52 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
+function openMenu() {
+    document.getElementById("menu").style.width = "250px";
+}
 
+function closeMenu() {
+    document.getElementById("menu").style.width = "0";
+}
+
+
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('menu');
+    const menuBtn = document.querySelector('.menu-btn');
+    
+    if (!menu.contains(event.target) && event.target !== menuBtn && !menuBtn.contains(event.target)) {
+        closeMenu();
+    }
+});
